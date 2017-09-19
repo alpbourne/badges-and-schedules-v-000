bd4dcb5e68e48d05d72196611f9a3a68ecdfc89d
alpbourne <alpbourne@gmail.com>
2017-09-19 20:09:12 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,7 +12,8 @@ end
 def assign_rooms(attendees)
   room_assignments=[]
   attendees.each_with_index {|i, number| room_assignments << "Hello, #{i}! You'll be assigned to room #{number+1}!"}
-  room_assignments
+end
+  return room_assignments
 end
 
 def printer
